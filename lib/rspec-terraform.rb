0e870c9fe7c4f7b5b365b360f775e0233890a418
Ben Snape <ben.snape@itv.com>
2015-08-25 11:46:45 +0100

    added test for AWS provider test setup

--- lib/rspec-terraform.rb
+++ lib/rspec-terraform.rb
@@ -1,3 +1,12 @@
 require 'bundler/setup'
 
 require_relative 'rspec-terraform/version'
+
+RSpec.configure do |config|
+
+  config.before(:each, provider: :aws) do
+    ENV['TF_VAR_access_key'] = 'foo'
+    ENV['TF_VAR_secret_key'] = 'bar'
+  end
+
+end
--- spec/terraform_spec.rb
+++ spec/terraform_spec.rb
@@ -4,4 +4,9 @@ describe RSpec::Terraform do
   it 'has a version number' do
     expect(RSpec::Terraform::VERSION).not_to be nil
   end
+
+  it 'creates terraform environment variables for AWS providers', provider: :aws do
+    expect(ENV['TF_VAR_access_key']).to eq 'foo'
+    expect(ENV['TF_VAR_secret_key']).to eq 'bar'
+  end
 end
